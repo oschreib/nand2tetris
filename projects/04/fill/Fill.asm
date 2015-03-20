3cbf6e98293588dd5d2c7027cd379e16dedec874
kruvin <kruvin@hotmail.com>
2015-03-20 14:18:56 +0200

    implement proj04

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -3,9 +3,50 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input. 
+// Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+(LOOP)
+  @col
+  M = 0 // Set pixel color to WHITE
+  @KBD
+  D = M
+  @WHITE
+  D;JEQ // If D==0 then goto WHITE, else col=BLACK
+  @col
+  M = 0 // Set pixel color to BLACK (-1)
+  M = M - 1
+(WHITE)
+
+  @8192 // Last screen position
+  D = A
+  @i
+  M = D // i = 8192
+  @SCREEN
+  D = A
+  @pos
+  M = D // pos = SCREEN
+
+(PAINTLOOP)
+  @col
+  D = M // D = col (WHITE / BLACK)
+
+  @pos
+  A = M // A = pos
+  M = D // paint pixel the required color
+  @pos
+  M = M + 1
+  @i
+  M = M - 1 // i--
+  D = M // D = i
+  @PAINTLOOP
+  D;JGT // i > 0 Goto PAINTLOOP
+
+  @LOOP
+  0;JMP // Goto LOOP
+
+(END)
+  @END
+  0;JMP // Infinite loop
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -6,4 +6,28 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// Add R0+R0... R1 times.
+  @R2
+  M=0 // R2 = 0
+  @R1
+  D=M // D = R1
+  @R4
+  M=D // Store a copy of R1 in R4 so we don't change R1
+
+(LOOP)
+  @R4
+  D=M // D = R4
+  @END
+  D;JEQ // If R4 == 0 goto END
+  @R0
+  D=M // D = R0
+  @R2
+  M=D+M // R2 += R0
+  @R4
+  M=M-1 // R4--
+  @LOOP
+  0;JMP // Goto LOOP
+
+(END)
+  @END
+  0;JMP // Infinite loop
